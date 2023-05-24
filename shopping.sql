5d702a16cf3fee5d64ae70a23707ef7cb760a49e
nasiriubat <shuvo131662@gmail.com>
2023-05-24 21:15:05 +0600

    24 may

--- shopping.sql
+++ shopping.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 16, 2023 at 04:49 AM
+-- Generation Time: May 24, 2023 at 03:13 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -198,8 +198,8 @@ CREATE TABLE `slider` (
 --
 
 INSERT INTO `slider` (`id`, `image`) VALUES
-(14, 'free & easy.png'),
-(15, 'Untitled design (2).png');
+(15, 'Untitled design (2).png'),
+(16, 'danial-igdery-FCHlYvR5gJI-unsplash.jpg');
 
 -- --------------------------------------------------------
 
@@ -430,7 +430,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `slider`
 --
 ALTER TABLE `slider`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `subcategory`
